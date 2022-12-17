32032ec910bbdb6a961b7499b6947f0a39910d67
minhhieu0411 <120078681+minhhieu0411@users.noreply.github.com>
2022-12-17 12:00:56 +0700

    Tạo trang homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'Product' => $productRepository ->findAll()
+        ]);
+    }
+}
--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -12,15 +12,19 @@ class ProductFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $product = new Product();
-        $product->setProductTitle('ASD');
+        $titles = ["Mobile","TV","Laptop","Headphones"];
+        for ($i = 1; $i <= 20; $i++){
+        $key = array_rand($titles,1);
+        $product = new Product;
+        $product->setProductTitle($titles[$key]);
         $product->setProductDescription('QWEASDZXC');
         $product->setProductKeyword('IP');
-        $product->setProductImage('https://pixabay.com/link/?ua=cd3%3Dimage%26cd7%3Dvi%253Avietnam%253AVNM%26ec%3Dapi_ad%26ea%3Dnavigate%26el%3Dgetty%26tid%3DUA-20223345-1%26dr%3Dhttps%253A%252F%252Fwww.google.com%252F&next=https%3A%2F%2Fwww.istockphoto.com%2Fphoto%2Flandscape-with-halong-bay-gm1201281530-344439656%3Futm_source%3Dpixabay%26utm_medium%3Daffiliate%26utm_campaign%3DSRP_image_sponsored%26utm_content%3Dhttps%253A%252F%252Fpixabay.com%252Fvi%252Fimages%252Fsearch%252Fvietnam%252F%26utm_term%3Dvietnam&hash=9085e8967e20737dd433c4407c038cd547413591&=');
+        $product->setProductImage('Mobile.jpg');
         $product->setProductPrice('23');
         $product->setDate(\DateTime::createFromFormat('Y-m-d', '1995-05-25'));
         $product->setStatus("true");
         $manager->persist($product);
-        $manager->flush();
-    }
+    }   
+    $manager->flush();
+}
 }
