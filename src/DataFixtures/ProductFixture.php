4beb9b1218be915139602c3fba7256b72197f53b
minhhieu0411 <120078681+minhhieu0411@users.noreply.github.com>
2022-12-19 16:30:40 +0700

    Creating product detail

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -2,26 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use DateTimeInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Product;
 
 
 class ProductFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $titles = ["Mobile","TV","Laptop","Headphones"];
-        for ($i = 1; $i <= 20; $i++){
-        $key = array_rand($titles,1);
+        for ($i = 1; $i <= 10; $i++){
         $product = new Product;
-        $product->setProductTitle($titles[$key]);
-        $product->setProductDescription('QWEASDZXC');
-        $product->setProductKeyword('IP');
-        $product->setProductImage('Mobile.jpg');
-        $product->setProductPrice('23');
-        $product->setDate(\DateTime::createFromFormat('Y-m-d', '1995-05-25'));
+        $product->setProductTitle('Demo');
+        $product->setProductDescription('BLABLABLABLA');
+        $product->setProductKeyword('SS');
+        $product->setProductImage('https://tintuc.hoang-phuc.com/wp-content/uploads/2021/12/hinh-nen-dep-20.jpg');
+        $product->setProductPrice('40');
+        $product->setDate(\DateTime::createFromFormat('Y-m-d', '2020-10-15'));
         $product->setStatus("true");
         $manager->persist($product);
     }   
